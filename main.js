cda18f636b5d05cd8e2c0f354439fbb5533f208c
RudnickiPawel <rudnicki.pawel94@gmail.com>
2020-02-18 12:10:56 +0100

    setting up 'about' view, added underline of current category

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
     }
   });
 
-  //Header, radio buttons
+  //header, radio buttons
   $(".header__radio-button").on("click", function () {
     $(".header__radio-button circle").removeClass("header__radio-button--active");
     $(this).find("circle:last").addClass("header__radio-button--active");
